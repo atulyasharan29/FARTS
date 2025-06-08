eb31d4e41eb93e11676bd2b2cd9dcb87e83d20fa
atulyasharan29 <143794824+atulyasharan29@users.noreply.github.com>
2025-06-09 01:35:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -114,4 +114,7 @@ def tracker(callsign):
 
 
 if __name__ == '__main__':
-    app.run(debug=False )
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+    
+    
